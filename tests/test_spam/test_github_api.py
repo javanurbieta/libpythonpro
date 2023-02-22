b8a8560cfef9294095044521ad6382669b632bd2
javanurbieta <107963730+javanurbieta@users.noreply.github.com>
2023-02-22 06:50:25 -0300

    Isolamento de Imports

--- tests/test_spam/test_github_api.py
+++ tests/test_spam/test_github_api.py
@@ -1,16 +1,30 @@
 from unittest.mock import Mock
 
+import pytest
+
 from libpythonpro import github_api
 
-
-def test_buscar_avatar():
-    resp_mock=Mock()
-    resp_mock.json.return_value={
+@pytest.fixture
+def avatar_url():
+    resp_mock = Mock()
+    url = 'https://avatars.githubusercontent.com/u/107963730?v=4'
+    resp_mock.json.return_value = {
         'login': 'javanurbieta', 'id': 107963730,
-        'avatar_url': 'https://avatars.githubusercontent.com/u/107963730?v=4'
+        'avatar_url': url,
     }
+    get_original = github_api.requests.get
     github_api.requests.get = Mock(return_value=resp_mock)
+    yield url
+    github_api.requests.get = get_original
 
+
+def test_buscar_avatar(avatar_url):
+    url = github_api.buscar_avatar('javanurbieta')
+    assert avatar_url == url
+
+
+
+def test_buscar_avatar_integracao():
     url = github_api.buscar_avatar('javanurbieta')
     assert 'https://avatars.githubusercontent.com/u/107963730?v=4' == url
 
